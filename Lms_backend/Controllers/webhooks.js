781514c4fec35ee7f4b3cfa9e34712bcb65ee8da
AniketChavan07 <aniketchavan00981@gmail.com>
2026-06-15 18:18:11 +0530

    webhooks in backend

--- Lms_backend/Controllers/webhooks.js
+++ Lms_backend/Controllers/webhooks.js
@@ -1,55 +1,59 @@
-import {Webhook} from 'svix'
-import User from '../models/User.js'
+import { Webhook } from "svix";
+import User from "../models/User.js";
 
-// api controller function to manage clerk user with the database
+export const clerkhooks = async (req, res) => {
+  try {
+    const webhook = new Webhook(process.env.CLERK_WEB_HOOK);
 
-export const clerkhooks = async(req,res) =>{
-    try {
-        const whook = new Webhook(process.env.CLERK_WEB_HOOK);
-        await whook (JSON.stringify(req.body),{
-            "svix-id" : req.headers["svix-id"],
-            "svix-timestamp" : req.headers["svix-timestamp"],
-            "svix-signature" : req.headers["svix-signature"]
-        })
+    const payload = webhook.verify(
+      JSON.stringify(req.body),
+      {
+        "svix-id": req.headers["svix-id"],
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"],
+      }
+    );
 
+    const { data, type } = payload;
 
-        const {data,type} = req.body
-         switch (type) {
-            case 'user.created':{
-                const userdata ={
-                    _id : data.id,
-                    email :data.email_address[0].email_address,
-                    name : data.first_name +" " +data.last_name,
-                    imgurl : data.image_url,
-                }
-                await User.create(userdata)
-                res.json({})
-                break;
-            }
-                
-             case 'user.updated':{
-                const userdata ={
-                    email :data.email_address[0].email_address,
-                    name : data.first_name +" " +data.last_name,
-                    imgurl : data.image_url,
-                }
-                await User.findByIdAndUpdate(data.id,userdata)
-                res.json({})
-                break;
-            }
-             case 'user.delete':{
-              
-                await User.findByIdAndDelete(data.id)
-                res.json({})
-                break;
-            }
-                break;
-         
-            default:
-                break;
-         }
-        
-    } catch (error) {
-        res.json({message:"error"})
+    switch (type) {
+      case "user.created": {
+        const userdata = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imgurl: data.image_url,
+        };
+
+        await User.create(userdata);
+        return res.json({ success: true });
+      }
+
+      case "user.updated": {
+        const userdata = {
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imgurl: data.image_url,
+        };
+
+        await User.findByIdAndUpdate(data.id, userdata);
+        return res.json({ success: true });
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        return res.json({ success: true });
+      }
+
+      default:
+        return res.json({ success: true });
     }
-}
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
